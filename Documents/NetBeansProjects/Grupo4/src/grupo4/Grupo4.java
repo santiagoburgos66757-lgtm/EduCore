5b7b5946ae8d03ce7c30758de96fa681a030a5e0
santiagoburgos66757-lgtm <santiago.burgos66757@ucaldas.edu.co>
2026-05-05 20:21:27 -0500

    primer comentario del proyecton 3

--- Documents/NetBeansProjects/Grupo4/src/grupo4/Grupo4.java
+++ Documents/NetBeansProjects/Grupo4/src/grupo4/Grupo4.java
@@ -14,7 +14,7 @@ public class Grupo4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLAAAA EQUIPO si");
+        System.out.println("HOLAAAA EQUIPO siiii");
     }
     
 }
